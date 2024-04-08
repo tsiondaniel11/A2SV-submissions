9cb8c718112619ea8c6ccf5b25fe65b08eccdd98
tsiondaniel11 <160366380+tsiondaniel11@users.noreply.github.com>
2024-04-09 01:17:06 +0300

    Create How Many Numbers Are Smaller Than The Current Number.py

---
+++ How Many Numbers Are Smaller Than The Current Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range(len(nums)):
+            count = 0
+            for j in range(len(nums)):
+                if j!=i:
+                   if nums[i] >nums[j]:
+                    count += 1
+
+            ans.append(count)
+        return ans
