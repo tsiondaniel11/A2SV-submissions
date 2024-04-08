da82f20ee9bb8a736337515365e903a63ece3fb2
tsiondaniel11 <160366380+tsiondaniel11@users.noreply.github.com>
2024-04-08 22:40:58 +0300

    Create Longest common  prefix.py

---
+++ Longest common  prefix.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        ans = ""
+        str = sorted(strs)
+        first = str[0]
+        last = str[-1]
+        for i in range(min(len(first),len(last))):
+            if first[i] != last[i]:
+                return ans 
+            ans+= first[i]
+        return ans
