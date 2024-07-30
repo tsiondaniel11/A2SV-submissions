0e358464f5a9b33220eb8b1e9634d98a3fc267d2
tsiondaniel11 <160366380+tsiondaniel11@users.noreply.github.com>
2024-07-30 10:38:58 +0300

    Update Find common characters.py

--- Find common characters.py
+++ Find common characters.py
@@ -4,3 +4,17 @@ class Solution:
        for i in words:
             res = res & Counter(i)
        return list(res.elements())
+
+
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        cnt = Counter(words[0])
+        for w in words:
+            curr_cnt = Counter(w)
+            for c in cnt:
+                cnt[c] = min(cnt[c],curr_cnt[c])
+        res =[]
+        for c in cnt:
+            for i in range(cnt[c]):
+                res.append(c)
+        return res
