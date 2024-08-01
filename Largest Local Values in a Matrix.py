530bcb9e9fd0f00a5098a0c18cf264cf60ac3227
tsiondaniel11 <160366380+tsiondaniel11@users.noreply.github.com>
2024-08-01 09:51:00 +0300

    Update Largest Local Values in a Matrix.py

--- Largest Local Values in a Matrix.py
+++ Largest Local Values in a Matrix.py
@@ -12,3 +12,16 @@ class Solution:
                 temp_row.append(max_value)
             max_local.append(temp_row)
         return max_local
+
+
+class Solution:
+    def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        n = len(grid)
+        res =[[0]*(n-2) for _ in range(n-2)]
+        for i in range(n-2):
+            for j in range(n-2):
+                for r in range(i,i+3):
+                    for c in range(j,j+3):
+                        res[i][j] =max(res[i][j],
+                                       grid[r][c])
+        return res 
